57eae8f197d1f3bcf9c7c91f97bedf74ed5459a3
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-09-02 21:42:28 +0200

    esercizio completato

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
-//Creiamo il numero casuale
-for (i=0; i<16; i++) {
+let listaBombe = [];            //array che contiene le bombe
+let numeroBombe = 0;            //numero di bombe del computer 
+let tettoMaxBombe = 100;        //numero massimo di randomizzazione per le bombe
 
-    let numeroCasualeComputer = getRandomInt(100);
-    console.log(numeroCasualeComputer);
+let difficoltà = prompt('scegli tra: facile, medio e difficile');
+let difficoltàPossibili = [ 'facile', 'medio', 'difficile' ];
+
+
+switch (difficoltà) {
+        case 'facile':
+            numeroBombe = 5;
+            tettoMaxBombe = 100;
+            break;
+            
+        case 'medio':
+            numeroBombe = 10;
+            tettoMaxBombe = 100;
+            break;  
+            
+        case 'difficile':
+            numeroBombe = 20;
+            tettoMaxBombe = 100;
+            break;
+        
+        default:
+            numeroBombe = 5;
+            tettoMaxBombe = 100;
+    }
+
+let numeroUtente = 0;
+
+let listaNumeriUtente = [];     //array conn i numeri dell'utente
+
+while (listaBombe.length < numeroBombe){
+    if(!listaBombe.includes(listaBombe)) //condizione per far sì che il pc non ripeta numeri
+    listaBombe.push(getRandomNumber(1,tettoMaxBombe));
+};
+
+console.log(listaBombe);
+
+while (listaNumeriUtente.length < numeroBombe) {
+    let numeroUtente = parseInt(prompt('scrivi una mossa'));
+    if (listaBombe.includes(numeroUtente)==true){
+        alert('hai perso')
+    } else {
+       listaNumeriUtente.push(numeroUtente);
+    }
+}
+
+console.log(listaNumeriUtente);
+
+
+//funzione che genera un numero random tra 2 valori
+function getRandomNumber (min,max) {
+    return Math.floor(Math.random()*(max - min + 1)) + min;
 
 }
 
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
